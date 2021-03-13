372c15276375412daf7bee98136658cbcfea715d
Italo Alves <contato.italo1020@gmail.com>
2021-03-13 11:54:31 -0300

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Slider from "@react-native-community/slider";
+import React, { useState } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Logo from "./src/assets/logo.png";
 
 export default function App() {
+  const [lenghtPassword, setLenghtPassword] = useState(0);
+  const [password, setPassword] = useState("");
+  const generatePassword = () => {
+    const characteres =
+      "abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let pass = "";
+    for (let i = 0, n = characteres.length; i < lenghtPassword; i++) {
+      pass += characteres.charAt(Math.floor(Math.random() * n));
+    }
+    setPassword(pass);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={Logo} style={{ marginBottom: 60 }} />
+      <Text style={styles.label}>{`${lenghtPassword} characters`}</Text>
+
+      <View style={styles.area}>
+        <Slider
+          style={{ height: 50 }}
+          minimumValue={5}
+          maximumValue={20}
+          minimumTrackTintColor="#FF0000"
+          maximumTrackTintColor="#000"
+          onValueChange={(value) => setLenghtPassword(parseInt(value))}
+        />
+      </View>
+      <TouchableOpacity onPress={generatePassword}>
+        <Text style={styles.btnGenerate}>Generate</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.messageYouPassword}>Your password is:</Text>
+      <View style={styles.passwordArea}>
+        <Text style={styles.password}>{password}</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +45,44 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#f3f3ff",
+  },
+  label: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "#010101",
+  },
+  area: {
+    marginTop: 15,
+    marginBottom: 15,
+    backgroundColor: "#FFF",
+    width: "90%",
+    borderRadius: 7,
+  },
+  btnGenerate: {
+    fontSize: 25,
+    backgroundColor: "#f2b61d",
+    borderRadius: 15,
+    color: "#FFFF",
+    paddingHorizontal: 100,
+    marginTop: 20,
+  },
+  password: {
+    fontSize: 25,
+  },
+  passwordArea: {
+    width: "80%",
+    height: 70,
+    borderRadius: 7,
+    backgroundColor: "#FFFF",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
+  },
+  messageYouPassword: {
+    marginTop: 40,
+    fontSize: 15,
   },
 });
